56e57af40cd14a56da74c51e6e0bd5b09722896b
Pamela Luque <pamela.luque@pucp.edu.pe>
2021-04-28 11:47:52 -0500

    preg 2

--- src/main/java/com/example/laboratorio3/entity/Employees.java
+++ src/main/java/com/example/laboratorio3/entity/Employees.java
@@ -3,6 +3,7 @@ package com.example.laboratorio3.entity;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "employees")
@@ -17,52 +18,12 @@ public class Employees {
     private String phone_number;
     private double salary;
 
+    @ManyToOne
+    @JoinColumn(name = "job_id")
+    private Job job;
 
-    public int getEmployee_id() {
-        return employee_id;
-    }
+    @OneToMany
+    @JoinColumn(name = "department_id")
+    private List<Departments>;
 
-    public void setEmployee_id(int employee_id) {
-        this.employee_id = employee_id;
-    }
-
-    public String getFirst_name() {
-        return first_name;
-    }
-
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-    }
-
-    public String getLast_name() {
-        return last_name;
-    }
-
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone_number() {
-        return phone_number;
-    }
-
-    public void setPhone_number(String phone_number) {
-        this.phone_number = phone_number;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
 }
